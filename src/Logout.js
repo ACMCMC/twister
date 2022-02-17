8e6a1d187aa8cf9b735469a828a84116d588b4c8
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2022-02-17 19:06:10 +0100

    Logout

---
+++ src/Logout.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Logout extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<div>Logout</div>);
+    }
+}
+
+export {Logout};
