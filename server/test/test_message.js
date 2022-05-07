8db22ce097a81404da56ae265da46ada476fb45a
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2022-05-07 14:51:10 +0200

    tests

--- server/test/test_message.js
+++ server/test/test_message.js
@@ -11,6 +11,7 @@ chai.use(chaiHttp);
 chai.should();
 
 var sid;
+var message_id;
 
 mocha.describe("Test of the messages API", () => {
 
@@ -40,7 +41,6 @@ mocha.describe("Test of the messages API", () => {
         const req = chai.request(app.default).get('/api/public/get_session_status');
         req.cookies = sid;
         return req.then((res) => {
-            console.log(res.body);
             chai.expect(res).to.have.status(200);
             chai.expect(res.body).to.contain({ logged_in: true });
         }
@@ -59,6 +59,50 @@ mocha.describe("Test of the messages API", () => {
         request.send(body)
             .then((res) => {
                 res.should.have.status(201);
+                message_id = res.body._id;
+                done();
+            }).catch((err) => done(err)).finally(() => chai.request().close());
+    })
+
+    mocha.it("Get message", (done) => {
+        const request = chai.request(app.default).get('/api/message');
+        const query = {
+            _id: message_id
+        };
+
+        request.cookies = sid;
+        request.query(query)
+            .then((res) => {
+                res.should.have.status(200);
+                res.body.should.contain({text: "ABCDEF"})
+                done();
+            }).catch((err) => done(err)).finally(() => chai.request().close());
+    })
+
+    mocha.it("Get all messages", (done) => {
+        const request = chai.request(app.default).get('/api/message/all');
+
+        request.cookies = sid;
+        request.then((res) => {
+                res.should.have.status(200);
+                chai.expect(res.body).to.be.an('array');
+                chai.expect(res.body).to.satisfy((array) => {
+                    return array.some((e) => e['text'] === "ABCDEF");
+                });
+                done();
+            }).catch((err) => done(err)).finally(() => chai.request().close());
+    })
+
+    mocha.it("Delete message", (done) => {
+        const request = chai.request(app.default).delete('/api/message');
+        const query = {
+            _id: message_id
+        };
+
+        request.cookies = sid;
+        request.query(query)
+            .then((res) => {
+                res.should.have.status(200);
                 done();
             }).catch((err) => done(err)).finally(() => chai.request().close());
     })
